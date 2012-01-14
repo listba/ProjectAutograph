c0c5707c873af1e4884558000ae773cc54e3c1f9
Keith Wentzel <wentzekm@gmail.com>
2012-01-13 22:34:32 -0500

    Added graph, edge, node, and error handler classes

---
+++ Graph.java
@@ -0,0 +1,74 @@
+
+package autograph;
+import java.util.ArrayList;
+import java.io.Serializable;
+/**
+ * Graph contains the data and functionality for building graph objects to be represented by Autograph
+ *
+ * @author Keith Wentzel
+ * @version 1.0
+ */
+public class Graph implements Serializable{
+   private ArrayList vNodeList;
+   private ArrayList vEdgeList;
+
+   /**
+    * AddEdge - adds an edge to the edge list for the graph
+    *
+    * @param edge - edge to be added to the edge list
+    * @return - ErrorHandler.NOERROR if successful, other error code if not successful
+    * @see Edge
+    */
+   public int AddEdge(Edge edge){
+      int retValue = ErrorHandler.CANNOTADDEDGE;
+      if(vEdgeList.add(edge)){
+         retValue = ErrorHandler.NOERROR;
+      }
+      return retValue;
+   }
+
+   /**
+    * RemoveEdge - removes an edge from the edge list for the graph
+    *
+    * @param edge - edge to be removed from the edge list
+    * @return - ErrorHandler.NOERROR if successful, other error code if not successful
+    * @see Edge
+    */
+   public int RemoveEdge(Edge edge){
+      int retValue = ErrorHandler.CANNOTREMOVEEDGE;
+      if(vEdgeList.remove(edge)){
+         retValue = ErrorHandler.NOERROR;
+      }
+      return retValue;
+   }
+
+   /**
+    * AddNode - adds a node to the edge list
+    *
+    * @param node - node to add to the node list for the graph
+    * @return - ErrorHandler.NOERROR if successful, other error code if not successful
+    * @see Node
+    */
+   public int AddNode(Node node){
+      int retValue = ErrorHandler.CANNOTADDNODE;
+      if(vNodeList.add(node)){
+         retValue = ErrorHandler.NOERROR;
+      }
+      return retValue;
+   }
+
+   /**
+    * RemoveNode - removes a node from the node list
+    *
+    * @param node - node to remove from the node list for the graph
+    * @return - ErrorHandler.NOERROR if successful, other error code if not successful
+    * @see Node
+    */
+   public int RemoveNode(Node node){
+      int retValue = ErrorHandler.CANNOTREMOVENODE;
+      if(vEdgeList.remove(node)){
+         retValue = ErrorHandler.NOERROR;
+      }
+      return retValue;
+   }
+}
---
+++ Node.java
@@ -0,0 +1,168 @@
+package autograph;
+
+import java.util.ArrayList;
+import java.io.Serializable;
+/**
+ * Node contains all data for node objects of a Graph.
+ *
+ * @author Keith Wentzel
+ * @version 1.0
+ */
+public class Node implements Serializable {
+
+   //enumerations
+   /**
+    * NodeShape - the various shapes for the border of the node
+    */
+   public enum NodeShape{
+      SQUARE,
+      CIRCLE,
+      TRIANGLE,
+      RECTANGLE,
+      OVAL
+   };
+   /**
+    * NodeStyle - the various styles for the border of the node
+    */
+   public enum NodeStyle{
+     SOLID,
+     DOTTED,
+     DASHED,
+     DOUBLE
+   }
+
+   //Member Variables
+   private String vName;
+   private String  vLabel;
+   private NodeShape vShape;
+   private NodeStyle vStyle;
+   private ArrayList vEdges;
+
+   /**
+    * Node Constructor
+    *
+    * Set up the node object based on  user input
+    *
+    * @param name variable name to use for accessing the node
+    * @param label label to be displayed in the node
+    * @param shape shape of the node
+    * @param style style of the node
+    * @see NodeShape
+    * @see NodeStyle
+    */
+   public Node(String name, String label, NodeShape shape, NodeStyle style){
+      vName = name;
+      vLabel = label;
+      vShape = shape;
+      vStyle = style;
+      //For now when we create a node we will not have any edge data. This may
+      //change at some point.
+      vEdges = new ArrayList();
+   }
+
+
+   /**
+    * GetName - used to access the variable name of the node to differentiate
+    * from other nodes
+    * @return the variable name of the node
+    */
+   public String GetName(){
+      return vName;
+   }
+
+   /**
+    * SetLabel - change the label on the node
+    * @param label string to change the label to
+    */
+   public void SetLabel(String label){
+      vLabel = label;
+   }
+
+
+   /**
+    * GetLabel - gets the labbel of the current node
+    * @return vLabel
+    */
+   public String GetLabel(){
+      return vLabel;
+   }
+
+
+   /**
+    * SetShape - sets the shape of the node
+    * @param shape is an enumaration of the shape of the node
+    * @see NodeShape
+    */
+   public void SetShape(NodeShape shape){
+      vShape = shape;
+   }
+
+
+   /**
+    * GetShape - gets the shape of the node
+    * @return an enumeration of the shape of the node
+    * @see NodeShape
+    */
+   public NodeShape GetShape(){
+      return vShape;
+   }
+
+
+   /**
+    * SetStyle - sets the style of the node
+    * @param style an enumeration of the style of the node
+    * @see NodeStyle
+    */
+   public void SetStyle(NodeStyle style){
+      vStyle = style;
+   }
+
+
+   /**
+    * GetStyle - gets the style of the node
+    * @return an enumeration of the style of the node
+    * @see NodeStyle
+    */
+   public NodeStyle GetStyle(){
+      return vStyle;
+   }
+
+
+   /**
+    * AddEdge - adds an edge to the edge list for the node
+    * @param edge - the edge to add to the list
+    * @return ErrorHandler.CANNOTADDEDGE if successful, other error code if not successful
+    * @see Edge
+    */
+   public int AddEdge(Edge edge){
+      int retValue = ErrorHandler.CANNOTADDEDGE;
+      if(vEdges.add(edge)){
+         retValue = ErrorHandler.NOERROR;
+      }
+      return retValue;
+   }
+
+
+   /**
+    * RemoveEdge - removes an edge from the edge list for the node
+    * @param edge - the edge to remove from the list
+    * @return ErrorHandler.NOERROR if successful, other error code if not successful
+    * @see Edge
+    */
+   public int RemoveEdge(Edge edge){
+      int retValue = ErrorHandler.CANNOTREMOVEEDGE;
+      if(vEdges.remove(edge)){
+         retValue = ErrorHandler.NOERROR;
+      }
+      return retValue;
+   }
+
+
+   /**
+    * GetEdgeList - returns a list of edges for the node
+    * @return - vEdges
+    */
+   public ArrayList GetEdgeList(){
+      return vEdges;
+   }
+}
