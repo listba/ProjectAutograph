90af840e4e22c3bf64622766f7b2495e1dfe084a
Jeff Farris <jeff.markues@gmail.com>
2012-04-14 20:31:00 -0400

    Cleaned up GUI code, Add Node and Add Edge Buttons working (brings up relevant side panel) Add Node button in new panel is working. There's going to need to be a pretty large GUI code overhaul here soon...autogenerated code is not cutting it... :X

--- autograph/Autograph.java
+++ autograph/Autograph.java
@@ -17,19 +17,9 @@ public class Autograph extends mainWindow {
 
 	public Autograph(String title) {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// Create New Graph Panel
-		// And Load in graph from XML file
-		//String filePath = "Graph.xml";
-		//Graph graph = GraphHelper.mImportGraphFromXML(filePath);
-		//GraphPanel panel = new GraphPanel(graph);
-		// Write Graph to XML file
-		/*
-		String writeTo = "NewGraph.xml";
-		GraphHelper.mExportGraphToXML(graph, writeTo);
-		 */
-		// Draw each tab we have.
-		for(int i = 0; i < MainWindowTabbedPane.getTabCount()/*vTabs.size()*/; i++){
-			GraphPanel graphPanel = (GraphPanel)(/*vTabs.get(i)*/((JScrollPane)MainWindowTabbedPane.getComponentAt(i)).getViewport().getView());
+
+		for(int i = 0; i < MainWindowTabbedPane.getTabCount(); i++){
+			GraphPanel graphPanel = (GraphPanel)(((JScrollPane)MainWindowTabbedPane.getComponentAt(i)).getViewport().getView());
 			int imageWidth = GraphHelper.mGetPreferredImageWidth(graphPanel.mGetGraph());
 
 			graphPanel.setPreferredSize(new Dimension(imageWidth, imageWidth));
@@ -39,16 +29,6 @@ public class Autograph extends mainWindow {
 				GraphHelper.mDrawForceDirectedGraph(graphPanel);
 			}
 		}
-
-
-		//GraphHelper.mSavePNG(panel);
-		//try{
-		//GraphHelper.mExportGraphToGML(graph, "graphGML.txt", null);
-		//}
-		//catch(Exception e){
-
-		//}
-
 	}
 
 	public static void main(String[] args) {
--- autograph/Graph.java
+++ autograph/Graph.java
@@ -10,283 +10,283 @@ import autograph.exception.*;
  * @version 1.0
  */
 public class Graph implements Serializable {
-   private ArrayList<Node> vNodeList;
-   private ArrayList<Edge> vEdgeList;
-   private String vTitle;
-   public SelectedItems vSelectedItems;
+	private ArrayList<Node> vNodeList;
+	private ArrayList<Edge> vEdgeList;
+	private String vTitle;
+	public SelectedItems vSelectedItems;
 
-   /**
-    * ValidateGraph - ensures valid data is passed to the graph constructor
-    *
-    * @param title - title of the graph
-    */
-   private void mValidateGraph(String title) throws CannotCreateGraphException{
-      if(title == null){
-         throw new CannotCreateGraphException("The graph must have a title.");
-      }
-   }
-   
-   public Graph(String title){
-      try{
-         mValidateGraph(title);
-         vNodeList = new ArrayList<Node>();
-         vEdgeList = new ArrayList<Edge>();
-         vSelectedItems = new SelectedItems();
-         if(title.isEmpty()){
-            vTitle = "New Graph";
-         }
-         else{
-            vTitle = title;
-         }
-      }
-      catch(CannotCreateGraphException e){
-    	//TODO: This may need changed to an error file, etc
-    	  System.out.println("Error while trying to create graph: " + e.getError());
-      }
-   }
-   
-   /**
-    * GetNodeList - gets a list of nodes for the graph
-    *
-    * @return - node list for the graph
-    * @see Node
-    */
-   public ArrayList<Node> mGetNodeList(){
-      return vNodeList;
-   }
-   
-   /**
-    * GetEdgeList - gets an edge list for the graph
-    *
-    * @return a list of edges for the graph
-    * @see Edge
-    */
-   public ArrayList<Edge> mGetEdgeList(){
-      return vEdgeList;
-   }
-   
-   /**
-    * Returns title of Graph
-    *
-    * @return  The title of the graph
-    */
-   public String mGetTitle(){
-      return vTitle;
-   }
-   
-   /**
-    * Sets title of Graph
-    *
-    * @param title - the new title of the graph
-    */
-   public void mSetTitle(String title){
-      if (title !=null && !title.isEmpty()) {
-         vTitle = title;
-      }
-   }
-   
-   /**
-    * GetNodeById - returns the node with the selected Id
-    *
-    * @param id - id of the node 
-    * @return a node with the selected id, or null if the node is not found
-    * @see Node
-    */
-   public Node mGetNodeById(String id){
-      //initialize to null. It will be up to the caller to make sure the value is not
-      //null when it is returned.
-      Node node = null;
-      try{
-         for(int i = 0; i < vNodeList.size(); i++){
-            if(vNodeList.get(i).mGetId().equals(id)){
-               node = vNodeList.get(i);
-            }
-         }
-      }
-      catch(Exception e){
-         //This will theoretically only happen if vNodeList.get(i) is null.
-    	 //TODO: This may need changed to an error file, etc
-    	  System.out.println("Error while trying to find node: " + e.getMessage());
-      }
-      return node;
-   }
-   
-   /**
-    * DeleteNodeById - removes a node with the selected id from the node list for the graph
-    *
-    * @param id - id of the Node
-    * @see Node
-    */
-   public void mDeleteNodeById(String id) throws CannotRemoveNodeException {
-      try{
-         for(int i = 0; i < vNodeList.size(); i++){
-            if(vNodeList.get(i).mGetId().equals(id)){
-               //remove will remove the node and shift left, so it will not create
-               //a null entry in the list.
-               vNodeList.remove(i);
-            }
-         }
-      }
-      catch(Exception e){
-         //this will theoretically only happen if vNodeList.get(i) is null
-    	//TODO: This may need changed to an error file, etc
-    	  System.out.println("Error while trying to find node for deletion: " + e.getMessage());
-    	  
-    	  throw new CannotRemoveNodeException("Could not find node for deletion!", e);
-      }
-   }
-   
-   /**
-    * GetEdgeById - returns an edge with the selected id in the graph
-    *
-    * @param id - id of the Edge
-    * @return an edge with the selected id, or null if the node is not found
-    * @see Edge
-    */
-   public Edge mGetEdgeById(String id){
-      Edge edge = null;
-      try{
-         for(int i = 0; i < vEdgeList.size(); i++){
-            if(vEdgeList.get(i).mGetId().equals(id)){
-               edge = vEdgeList.get(i);
-            }
-         }
-      }
-      catch(Exception e){
-         //This will theoretically only happen if vNodeList.get(i) is null.
-    	//TODO: This may need changed to an error file, etc
-    	  System.out.println("Error while trying to find edge: " + e.getMessage());
-      }
-      return edge;
-   }
-   
-   /**
-    * DeleteEdgeById - removes the edge with the selected id from the graph
-    *
-    * @param id of the edge
-    * @see Edge
-    */
-   public void mDeleteEdgeById(String id) throws CannotRemoveEdgeException {
-      try{
-         for(int i = 0; i < vEdgeList.size(); i++){
-            if(vEdgeList.get(i).mGetId().equals(id)){
-               //remove will remove the node and shift left, so it will not create
-               //a null entry in the list.
-               vEdgeList.remove(i);
-            }
-         }
-      }
-      catch(Exception e){
-         //this will theoretically only happen if vNodeList.get(i) is null
-    	//TODO: This may need changed to an error file, etc
-    	  System.out.println("Error while trying to find edge for deletion: " + e.getMessage());
-    	  
-    	  throw new CannotRemoveEdgeException("Could not find edge for deletion!", e);
-      }
-   }
-   
-   /**
-    * AddEdge - adds an edge to the edge list for the graph
-    *
-    * @param edge - edge to be added to the edge list
-	* @see Edge
-	* 
-    */
-   public void mAddEdge(Edge edge) throws CannotAddEdgeException {
-      //TODO: link this function to the Add Edge button of the UI
-      //TODO: pass in or retrieve the graphics object in the JPanel,
-      //      clear the current image and call GraphHelper.mDrawGraph 
-      //      on the "this" object to rearrange the nodes with the new edge.
-	   try {
-		   vEdgeList.add(edge);
-	   }
-	   catch (Exception e) {
-		   throw new CannotAddEdgeException("Error while adding edge!", e);
-	   }
-   }
+	/**
+	 * ValidateGraph - ensures valid data is passed to the graph constructor
+	 *
+	 * @param title - title of the graph
+	 */
+	private void mValidateGraph(String title) throws CannotCreateGraphException{
+		if(title == null){
+			throw new CannotCreateGraphException("The graph must have a title.");
+		}
+	}
 
-   /**
-    * RemoveEdge - removes an edge from the edge list for the graph
-    *
-    * @param edge - edge to be removed from the edge list
-    * @see Edge
-    * 
-    */
-   public void mRemoveEdge(Edge edge) throws CannotRemoveEdgeException {
-      //TODO: link this function to the delete functionality of the UI
-      //TODO: pass in or retrieve the graphics object in the JPanel,
-      //      clear the current image and call GraphHelper.mDrawGraph 
-      //      on the "this" object after the edge is removed.
-	   try {
-		   vEdgeList.remove(edge);
-	   }
-	   catch (Exception e) {
-		   throw new CannotRemoveEdgeException("Error while removing edge!", e);
-	   }
-   }
+	public Graph(String title){
+		try{
+			mValidateGraph(title);
+			vNodeList = new ArrayList<Node>();
+			vEdgeList = new ArrayList<Edge>();
+			vSelectedItems = new SelectedItems();
+			if(title.isEmpty()){
+				vTitle = "New Graph";
+			}
+			else{
+				vTitle = title;
+			}
+		}
+		catch(CannotCreateGraphException e){
+			//TODO: This may need changed to an error file, etc
+			System.out.println("Error while trying to create graph: " + e.getError());
+		}
+	}
 
-   /**
-    * AddNode - adds a node to the node list - sets location as well
-    *
-    * @param node - node to add to the node list for the graph
-    * @param panel - the panel where the graph will be rendered
-    * @throws CannotAddNodeException
-    * @see Node
-    * 
-    */
-   public void mAddNode(GraphPanel panel, Node node) throws CannotAddNodeException {
-	   try {
-		   int width = panel.getWidth();
-		   int height = panel.getHeight();
+	/**
+	 * GetNodeList - gets a list of nodes for the graph
+	 *
+	 * @return - node list for the graph
+	 * @see Node
+	 */
+	public ArrayList<Node> mGetNodeList(){
+		return vNodeList;
+	}
 
-		   // Set each node at a random location
-		   // within the center 60% of the screen
-		   Random generator = new Random();
-		   double randX = generator.nextDouble() * (width - (width / 5)) + (width / 5);
-		   double randY = generator.nextDouble() * (height - (height / 5)) + (height / 5);
-		   node.mSetCenterLocation((int)randX, (int)randY);   
-		   vNodeList.add(node);
-	   }
-	   catch (Exception e) {
-		   throw new CannotAddNodeException("Error while adding node!", e);
-	   }
-   }
-   
-   /**
-    * AddNode - adds a node to the node list
-    * 
-    * @param node
-    * @throws CannotAddNodeException
-    * @see Node
-    * 
-    */
-   public void mAddNode(Node node) throws CannotAddNodeException {
-	   try {
-		   vNodeList.add(node);
-	   }
-	   catch (Exception e) {
-		   throw new CannotAddNodeException("Error while adding node!", e);
-	   }
-   }
+	/**
+	 * GetEdgeList - gets an edge list for the graph
+	 *
+	 * @return a list of edges for the graph
+	 * @see Edge
+	 */
+	public ArrayList<Edge> mGetEdgeList(){
+		return vEdgeList;
+	}
 
-   /**
-    * RemoveNode - removes a node from the node list
-    *
-    * @param node - node to remove from the node list for the graph
-    * @see Node
-    * 
-    */
-   public void mRemoveNode(Node node) throws CannotRemoveNodeException {
-      //TODO: link this function to the delete node functionality of the UI
-      //TODO: pass in or retrieve the graphics object in the JPanel,
-      //      clear the current image and call GraphHelper.mDrawGraph 
-      //      on the "this" object after the node is removed.
-      //TODO: remove any edges currently associated with this node.
-	   try {
-		   vNodeList.remove(node);
-	   }
-	   catch (Exception e) {
-		   throw new CannotRemoveNodeException("Error while removing node!", e);
-	   }
-   }
+	/**
+	 * Returns title of Graph
+	 *
+	 * @return  The title of the graph
+	 */
+	public String mGetTitle(){
+		return vTitle;
+	}
+
+	/**
+	 * Sets title of Graph
+	 *
+	 * @param title - the new title of the graph
+	 */
+	public void mSetTitle(String title){
+		if (title !=null && !title.isEmpty()) {
+			vTitle = title;
+		}
+	}
+
+	/**
+	 * GetNodeById - returns the node with the selected Id
+	 *
+	 * @param id - id of the node 
+	 * @return a node with the selected id, or null if the node is not found
+	 * @see Node
+	 */
+	public Node mGetNodeById(String id){
+		//initialize to null. It will be up to the caller to make sure the value is not
+		//null when it is returned.
+		Node node = null;
+		try{
+			for(int i = 0; i < vNodeList.size(); i++){
+				if(vNodeList.get(i).mGetId().equals(id)){
+					node = vNodeList.get(i);
+				}
+			}
+		}
+		catch(Exception e){
+			//This will theoretically only happen if vNodeList.get(i) is null.
+			//TODO: This may need changed to an error file, etc
+			System.out.println("Error while trying to find node: " + e.getMessage());
+		}
+		return node;
+	}
+
+	/**
+	 * DeleteNodeById - removes a node with the selected id from the node list for the graph
+	 *
+	 * @param id - id of the Node
+	 * @see Node
+	 */
+	public void mDeleteNodeById(String id) throws CannotRemoveNodeException {
+		try{
+			for(int i = 0; i < vNodeList.size(); i++){
+				if(vNodeList.get(i).mGetId().equals(id)){
+					//remove will remove the node and shift left, so it will not create
+					//a null entry in the list.
+					vNodeList.remove(i);
+				}
+			}
+		}
+		catch(Exception e){
+			//this will theoretically only happen if vNodeList.get(i) is null
+			//TODO: This may need changed to an error file, etc
+			System.out.println("Error while trying to find node for deletion: " + e.getMessage());
+
+			throw new CannotRemoveNodeException("Could not find node for deletion!", e);
+		}
+	}
+
+	/**
+	 * GetEdgeById - returns an edge with the selected id in the graph
+	 *
+	 * @param id - id of the Edge
+	 * @return an edge with the selected id, or null if the node is not found
+	 * @see Edge
+	 */
+	public Edge mGetEdgeById(String id){
+		Edge edge = null;
+		try{
+			for(int i = 0; i < vEdgeList.size(); i++){
+				if(vEdgeList.get(i).mGetId().equals(id)){
+					edge = vEdgeList.get(i);
+				}
+			}
+		}
+		catch(Exception e){
+			//This will theoretically only happen if vNodeList.get(i) is null.
+			//TODO: This may need changed to an error file, etc
+			System.out.println("Error while trying to find edge: " + e.getMessage());
+		}
+		return edge;
+	}
+
+	/**
+	 * DeleteEdgeById - removes the edge with the selected id from the graph
+	 *
+	 * @param id of the edge
+	 * @see Edge
+	 */
+	public void mDeleteEdgeById(String id) throws CannotRemoveEdgeException {
+		try{
+			for(int i = 0; i < vEdgeList.size(); i++){
+				if(vEdgeList.get(i).mGetId().equals(id)){
+					//remove will remove the node and shift left, so it will not create
+					//a null entry in the list.
+					vEdgeList.remove(i);
+				}
+			}
+		}
+		catch(Exception e){
+			//this will theoretically only happen if vNodeList.get(i) is null
+			//TODO: This may need changed to an error file, etc
+			System.out.println("Error while trying to find edge for deletion: " + e.getMessage());
+
+			throw new CannotRemoveEdgeException("Could not find edge for deletion!", e);
+		}
+	}
+
+	/**
+	 * AddEdge - adds an edge to the edge list for the graph
+	 *
+	 * @param edge - edge to be added to the edge list
+	 * @see Edge
+	 * 
+	 */
+	public void mAddEdge(Edge edge) throws CannotAddEdgeException {
+		//TODO: link this function to the Add Edge button of the UI
+		//TODO: pass in or retrieve the graphics object in the JPanel,
+		//      clear the current image and call GraphHelper.mDrawGraph 
+		//      on the "this" object to rearrange the nodes with the new edge.
+		try {
+			vEdgeList.add(edge);
+		}
+		catch (Exception e) {
+			throw new CannotAddEdgeException("Error while adding edge!", e);
+		}
+	}
+
+	/**
+	 * RemoveEdge - removes an edge from the edge list for the graph
+	 *
+	 * @param edge - edge to be removed from the edge list
+	 * @see Edge
+	 * 
+	 */
+	public void mRemoveEdge(Edge edge) throws CannotRemoveEdgeException {
+		//TODO: link this function to the delete functionality of the UI
+		//TODO: pass in or retrieve the graphics object in the JPanel,
+		//      clear the current image and call GraphHelper.mDrawGraph 
+		//      on the "this" object after the edge is removed.
+		try {
+			vEdgeList.remove(edge);
+		}
+		catch (Exception e) {
+			throw new CannotRemoveEdgeException("Error while removing edge!", e);
+		}
+	}
+
+	/**
+	 * AddNode - adds a node to the node list - sets location as well
+	 *
+	 * @param node - node to add to the node list for the graph
+	 * @param panel - the panel where the graph will be rendered
+	 * @throws CannotAddNodeException
+	 * @see Node
+	 * 
+	 */
+	public void mAddNode(GraphPanel panel, Node node) throws CannotAddNodeException {
+		try {
+			int width = panel.getWidth();
+			int height = panel.getHeight();
+
+			// Set each node at a random location
+			// within the center 60% of the screen
+			Random generator = new Random();
+			double randX = generator.nextDouble() * (width - (width / 5)) + (width / 5);
+			double randY = generator.nextDouble() * (height - (height / 5)) + (height / 5);
+			node.mSetCenterLocation((int)randX, (int)randY);   
+			vNodeList.add(node);
+		}
+		catch (Exception e) {
+			throw new CannotAddNodeException("Error while adding node!", e);
+		}
+	}
+
+	/**
+	 * AddNode - adds a node to the node list
+	 * 
+	 * @param node
+	 * @throws CannotAddNodeException
+	 * @see Node
+	 * 
+	 */
+	public void mAddNode(Node node) throws CannotAddNodeException {
+		try {
+			vNodeList.add(node);
+		}
+		catch (Exception e) {
+			throw new CannotAddNodeException("Error while adding node!", e);
+		}
+	}
+
+	/**
+	 * RemoveNode - removes a node from the node list
+	 *
+	 * @param node - node to remove from the node list for the graph
+	 * @see Node
+	 * 
+	 */
+	public void mRemoveNode(Node node) throws CannotRemoveNodeException {
+		//TODO: link this function to the delete node functionality of the UI
+		//TODO: pass in or retrieve the graphics object in the JPanel,
+		//      clear the current image and call GraphHelper.mDrawGraph 
+		//      on the "this" object after the node is removed.
+		//TODO: remove any edges currently associated with this node.
+		try {
+			vNodeList.remove(node);
+		}
+		catch (Exception e) {
+			throw new CannotRemoveNodeException("Error while removing node!", e);
+		}
+	}
 }
